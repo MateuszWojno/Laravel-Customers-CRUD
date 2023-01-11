8c352e72a4ccbe39d700aef9759cf44de3c9b2eb
MateuszWojno <mateusz.wojno9922@gmail.com>
2023-01-11 08:17:14 +0100

    Add CustomerController

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
