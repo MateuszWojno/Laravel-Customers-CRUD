8f74e0fd47ca9f54f2e8039db3cf72b5c488bc1f
MateuszWojno <mateusz.wojno9922@gmail.com>
2023-01-12 10:36:20 +0100

    Add CustomerFactory and CustomersTableSeeder to fill database data

---
+++ database/seeders/CustomersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use Illuminate\Database\Seeder;
+
+class CustomersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            Customer::factory(30)->create()
+        ]);
+    }
+}
